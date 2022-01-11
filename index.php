eb2db68bca0fae145878f9fa250b924e36949a45
DamienOlivier567 <73164252+DamienOlivier567@users.noreply.github.com>
2022-01-11 12:57:51 +0100

    fini

--- index.php
+++ index.php
@@ -2,32 +2,36 @@
 
 // Premiere ligne
 
-$fruits = array("Apple", "Banana" "Orange");
-echo ;
+$fruits = array("Apple", "Banana", "Orange");
+echo print_r($fruits)."<br>";
+echo $fruits[2];
 
 //Deuxieme ligne
 
 
-$age = array("Peter" "35", "Ben" "37", "Joe" "43");
+$age = array("Peter" => "35", "Ben" => "37", "Joe" => "43");
 
-echo "<br><br>Ben is " .  . " years old.<br><br>";
+echo "<br><br>Ben is " .$age["Ben"]  . " years old.<br><br>";
 
-($age  $x  $y) {
-    echo "<br>Key=" .  . ", Value=" . ;
+foreach($age as $x => $y) {
+    echo "<br>Key=" . $x  . ", Value=" . $y ;
 }
-
 
 
 //Troisieme ligne
 
 $colors = array("red", "green", "blue", "yellow");
-;
+sort($colors);
+echo ($colors);
+rsort($colors);
+echo ($colors);
 
 
 // Quatrieme ligne
 
 $age = array("Peter"=>"35", "Ben"=>"37", "Joe"=>"43");
-;
+krsort($age);
+echo ($age);
 
 
 
